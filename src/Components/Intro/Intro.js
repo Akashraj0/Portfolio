b182de68ba292c216e4ccab6981960be04e6a848
akashperumalraj0@gmail.com <akashperumalraj0@gmail.com>
2023-07-16 22:55:07 +0530

    Updated16

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -6,7 +6,7 @@ const Intro = () => {
             <h5 className='intro-head'>Get To Know</h5>
             <h4 className='intro-head1'>About Me</h4>
             <p className='intro-para'>HI!, I'm a Front-End Developer located in India, A Passinated programmer
-                <img src={require("./programmer.png")} alt='programmer' style={{ display: "inline" }} />
+                {/* <img src={require("./programmer.png")} alt='programmer' style={{ display: "inline" }} /> */}
                 who enjoys acquiring new Knowledge and using the skill to develop new things and a team player.
                 Interested in technologies and research. hobbies playing chess, surfing youtube,and playing basketball.</p>
         </div>
